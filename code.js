87df4b8e0c814fd41418813560581c7fbef939f2
Ravi Sharma <ravi.sharma26494@gmail.com>
2022-05-25 12:03:31 +0530

    I am adding all the files to my git hub Date:25/05.22

--- code.js
+++ code.js
@@ -14,8 +14,9 @@ let count = 0;
 for(let i = 0;i<nums.length; i++){
 	for(let j = i+1;j<nums.length; j++){
 		if(nums[i] == nums[j]){
-			count =count+1;
+			count = count+1;
 		}
 	}
 }
 console.log(count);
+// add all files to git
